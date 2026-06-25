9769e0a2b0c06794a6b6d97299dfa6c48d1ddd8e
TotoriYoyori <teainbasement@gmail.com>
2026-06-25 15:49:45 +0000

    <gold> items_recommend: done...this table is so dense

--- patch/20260625_add_orphan_item_id_under_ref.sql
+++ patch/20260625_add_orphan_item_id_under_ref.sql
@@ -2,27 +2,22 @@ USE DATABASE LEAGUE_RECORDS;
 
 USE SCHEMA BRONZE;
 -------------------------------------------------------------------------------------------
-    -- PROBLEM: There are 12 items referred to by MATCH_INTERVALS_BRONZE ITEM_0 -> ITEM_7 
-    -- that do not have corresponding reference rows under ITEMS_REF
-    -- This affects the following downstream transformation:
-    -- --> PLAYER_INTERVAL_SILVER
-    -- --> ITEMS_REF_SILVER
-
-    -- 2524, 3097, 2512, 8010, 2522, 3340, 2510, 2517, 2525, 2520, 2523, 2526 << Orphaned ITEM_ID
+    -- PROBLEM: There are 12 items referred to by MATCH_INTERVALS_SILVER ITEM_0 -> ITEM_7 
+    -- that do not have corresponding reference rows under ITEMS_REF_SILVER
 -------------------------------------------------------------------------------------------
 WITH ALL_PURCHASED_ITEMS_ID AS (
     SELECT
         MATCH_ID,
-        PLAYER_ID,
+        PARTICIPANT_POS_ID,
         ITEM.VALUE AS ITEM_ID
     FROM (
         SELECT 
             MATCH_ID,
-            PLAYER_ID,
+            PARTICIPANT_POS_ID,
             ARRAY_SORT(ARRAY_CONSTRUCT_COMPACT(
                 ITEM_0, ITEM_1, ITEM_2, ITEM_3, ITEM_4, ITEM_5, ITEM_6
             )) AS ITEM_ARRAY
-        FROM BRONZE.MATCH_INTERVALS_BRONZE
+        FROM SILVER.PLAYER_INTERVAL_SILVER
     ) AS BASE
     CROSS JOIN LATERAL FLATTEN(INPUT => BASE.ITEM_ARRAY) AS ITEM
     WHERE ITEM.VALUE != 0
@@ -30,13 +25,14 @@ WITH ALL_PURCHASED_ITEMS_ID AS (
 
 SELECT DISTINCT AP.ITEM_ID
 FROM ALL_PURCHASED_ITEMS_ID AS AP
-LEFT JOIN BRONZE.ITEMS_REF_BRONZE AS IR
+LEFT JOIN SILVER.ITEMS_REF_SILVER AS IR
     ON IR.ITEM_ID = AP.ITEM_ID
 WHERE IR.ITEM_NAME IS NULL
 ;
+-- 2524, 3097, 2512, 8010, 2522, 3340, 2510, 2517, 2525, 2520, 2523, 2526 << Orphaned ITEM_ID
+    
 -------------------------------------------------------------------------------------------
     -- SOLUTION: Manually discover and seed the 12 missing entries into ITEMS_REF_BRONZE 
-    -- after the original seed dataset. Using an INSERT() VALUES() query. 
     -- 01. Research what the items are corresponding to the orphaned ID.
     -- 02. Manual insert using INSERT() VALUES()
     -- 03. Bronze stream picks up delta
@@ -70,17 +66,16 @@ VALUES
     -- VERIFY: Verify that BRONZE.ITEMS_REF_BRONZE have received the change and all downstream
     -- propagations happen for this schema (ITEMS_REF_SILVER)
 -------------------------------------------------------------------------------------------
-SELECT *
-FROM BRONZE.ITEMS_REF_BRONZE
-ORDER BY LDTS DESC
-LIMIT 20
-;
-
-SELECT *
-FROM SILVER.ITEMS_REF_SILVER
-WHERE ITEM_ID IN (
-    2524, 3097, 2512, 8010, 2522, 
-    3340, 2510, 2517, 2525, 2520, 
-    2523, 2526
-)
+SELECT 
+    COUNT(B.ITEM_ID) AS BRONZE_ROWS_INSERTED,
+    COUNT(S.ITEM_ID) AS SILVER_ROWS_INSERTED,
+    COUNT(B.ITEM_ID) = COUNT(S.ITEM_ID) AS ALL_RECORDS_PROPAGATED
+FROM (
+    SELECT ITEM_ID
+    FROM BRONZE.ITEMS_REF_BRONZE 
+    WHERE RSRC = 'Manual backfill'
+        AND TO_CHAR(LDTS, 'YYYY-MM-DD') = '2026-06-25'
+) AS B
+JOIN SILVER.ITEMS_REF_SILVER AS S
+    ON S.ITEM_ID = B.ITEM_ID
 ;
